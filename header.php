fd42dd99fa02cad80befaa57669f90c7fe136d92
emquist <emil_96_malmquist@hotmail.com>
2019-06-09 01:13:43 +0200

    med stylesheet

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="description" content="This will often show up in search results">
     <meta name="viewport" content="width=device-width, initial scale=1">
-    <!--<link rel="stylesheet" type="text/css" href="style.css">-->
+    <link rel="stylesheet" type="text/css" href="Navigation.css">
     <title>Boker</title>
 </head>
 <body>
@@ -15,8 +15,8 @@
     <h1>Coolbooks1996</h1>
     <nav>
         <ul>
-            <li><a href="index.php">Start</a></li>
-            <li><a href="controls.php">Kontroller</a></li>
+            <li><a href="index.php">Hem</a></li>
+            <li><a href="controls.php">Mina böcker</a></li>
         </ul>
     </nav>
 </header>
