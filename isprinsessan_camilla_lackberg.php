3702d06b3feb132ee7155c1be120e31bd7c1a03e
emquist <emil_96_malmquist@hotmail.com>
2019-06-13 23:51:28 +0200

    Fungernade stjärnor och en bok template

--- isprinsessan_camilla_lackberg.php
+++ isprinsessan_camilla_lackberg.php
@@ -1,7 +1,21 @@
 <?php
 require "header.php";
 ?>
+<script type="text/javascript">
+    function switchRating() {
 
+        var rating = document.getElementById("rating");
+        var button = document.getElementById("ratingButton");
+        button.innerText=rating.style.display;
+        if(rating.style.display == "none" ||rating.style.display == ""||rating.style.display == null ){
+            rating.style.display = "block";
+            button.innerText = "betyg x av y röster, dölj?";
+        }else if(rating.style.display == "block") {
+            rating.style.display ="none";
+            button.innerText = "Visa betyg";
+        }
+    }
+</script>
 <main class="container">
     <link rel="stylesheet" type="text/css" href="css/bookpage.css">
     <div class="workspace">
@@ -16,9 +30,15 @@ require "header.php";
             <div class="summarise">
                 Huset var ödsligt och tomt. Kylan trängde in i alla vrår. En tunn hinna av is hade bildats i badkaret. Hon hade börjat anta en lätt blåaktig ton. Han tyckte att hon såg ut som en prinsessa där hon låg. En isprinsessa. Golvet han satt på var iskallt, men kylan bekymrade honom inte. Han sträckte ut handen och rörde vid henne. Blodet på hennes handleder hade för länge sedan stelnat. Kärleken till henne hade aldrig varit starkare. Han smekte hennes arm, som om han smekte den själ som nu flytt kroppen. Han vände sig inte om när han gick. Det var inte adjö, utan på återseende.I debutromanen Isprinsessan förlägger Camilla Läckberg handlingen vintertid till sin hemort Fjällbacka och låter morden växa fram ur småstadsandans sämre sidor. Hon tecknar ett porträtt av ett slutet samhälle där alla, på gott och ont, vet allt om varandra och där det yttre skenet har stor betydelse. Något som under fel omständigheter kan bli ödesdigert ...
             </div>
-            <div>
-                <button onclick="switchRating()" id="ratingButton">Visa betyg.</button>
-                <div class="rating">betyget</div>
+            <div id="ratingBox" >
+                <button type="button" onclick="switchRating()" id="ratingButton">Visa betyg</button>
+                <div class="rate" id="rating">
+                    <input type="radio" id="star5" name="rate" value="5"><label for="star5" title="Perfekt"></label>
+                    <input type="radio" id="star4" name="rate" value="4"><label for="star4" title="Bra"></label>
+                    <input type="radio" id="star3" name="rate" value="3"><label for="star3" title="Okej"></label>
+                    <input type="radio" id="star2" name="rate" value="2"><label for="star2" title="Inte så bra"></label>
+                    <input type="radio" id="star1" name="rate" value="1"><label for="star1" title="Väldigt dålig"></label>
+                </div>
             </div>
             <div class="commentfield">
                 Kommentarer:
@@ -36,23 +56,7 @@ require "header.php";
         <div class="col">
 
         </div>
-
     </div>
-    <script type="text/javascript">
-        function switchRating() {
-            var rating = document.getElementById("rating");
-            var button = document.getElementById("ratingButton")
-            if(rating.style.display == "none"){
-                rating.style.display = "block";
-                button.innerText = "Dölj betyg"
-            }else if(rating.style.display == "block") {
-                rating.style.display ="none";
-                button.innerText = "Visa betyg"
-            }
-        }
-
-
-    </script>
 </main>
 
 <?php
--- templates/book_template.php
+++ templates/book_template.php
@@ -2,6 +2,21 @@
 require "header.php";
 ?>
 
+<script type="text/javascript">
+    function switchRating() {
+
+        var rating = document.getElementById("rating");
+        var button = document.getElementById("ratingButton");
+        button.innerText=rating.style.display;
+        if(rating.style.display == "none" ||rating.style.display == ""||rating.style.display == null ){
+            rating.style.display = "block";
+            button.innerText = "Betyg x av y röster, dölj?";
+        }else if(rating.style.display == "block") {
+            rating.style.display ="none";
+            button.innerText = "Visa betyg";
+        }
+    }
+</script>
 <main class="container">
     <link rel="stylesheet" type="text/css" href="css/bookpage.css">
     <div class="workspace">
@@ -30,6 +45,16 @@ require "header.php";
                 Massor av text som står på baksidan.
                 Massor av text som står på baksidan.
                 Massor av text som står på baksidan.
+            </div>
+            <div id="ratingBox" >
+                <button type="button" onclick="switchRating()" id="ratingButton">Visa betyg</button>
+                <div class="rate" id="rating">
+                    <input type="radio" id="star5" name="rate" value="5"><label for="star5" title="Perfekt"></label>
+                    <input type="radio" id="star4" name="rate" value="4"><label for="star4" title="Bra"></label>
+                    <input type="radio" id="star3" name="rate" value="3"><label for="star3" title="Okej"></label>
+                    <input type="radio" id="star2" name="rate" value="2"><label for="star2" title="Inte så bra"></label>
+                    <input type="radio" id="star1" name="rate" value="1"><label for="star1" title="Väldigt dålig"></label>
+                </div>
             </div>
             <div class="commentfield">
                 Kommentarer:
