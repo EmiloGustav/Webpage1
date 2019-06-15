bca6269944812af57c5bdbee844ef4b4a2a716d8
Gustav <hultgrengustav@gmail.com>
2019-06-15 11:04:50 +0200

    Slideshow med bokomslag nästan färdig. Problem: Inget bokomslag visas från början utan man måste klicka antingen nästa eller föregående-knappen för att dem ska börja visas.

--- index.php
+++ index.php
@@ -4,65 +4,49 @@ require "header.php";
 <main class="container">
 
     <link rel="stylesheet" type="text/css" href="css/style.css">
-
-    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
     <script src="js/index.js"></script>
 
     <div class="workspace">
         <div class="col">
             <img src="https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="Books" width="426" height="250">
-            <img class="woman" src="images/woman_with_book.png" alt="" width="200px" height="200px">
         </div>
 
         <div class="col">
-            <h2>Hej</h2>
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
-            Massor med text.Massor med text.Massor med text.Massor med text.Massor med text.
+            <div class="slideshow-container">
+                <h2>Månadens populäraste böcker</h2>
+
+                <div class="mySlides fade">
+                    <div class="numbertext">1 / 4</div>
+                    <img src="images/greatgatsby.jpg" width="200px" height="300px">
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="numbertext">2 / 4</div>
+                    <img src="images/tokillamockingbird.jpg" width="200px" height="300px">
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="numbertext">3 / 4</div>
+                    <img src="images/harrypotter.jpg" width="200px" height="300px">
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="numbertext">4 / 4</div>
+                    <img src="images/fahrenheit.jpg" width="200px" height="300px">
+                </div>
+
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+
+            <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+                <span class="dot" onclick="currentSlide(4)"></span>
+            </div>
+
+
         </div>
 
         <div class="col">
@@ -98,6 +82,7 @@ require "header.php";
             </form>
         </div>
     </div>
+
 </main>
 <?php
 require "footer.php"
