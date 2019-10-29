5b46e18d22f12d85097b89a5e104e9d7559b6891
Gustav <hultgrengustav@gmail.com>
2019-10-29 12:01:27 +0100

    started to add php code to make the content dynamic

--- myBooksV2.php
+++ myBooksV2.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
 include 'includes/getDb.inc.php';
+if (isset($_SESSION['userId'])) {
+    $userinfo = getUserInfo($_SESSION['userId']);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,19 +23,39 @@ include 'includes/getDb.inc.php';
 <body>
     <aside>
         <figure>
-            <img id="logotype" src="images/books.png" alt="">
-            <figcaption>BonoLibro</figcaption>
+            <a href="index.php"><img id="logotype" src="images/books.png" alt=""></a>
+            <a href="index.php">
+                <figcaption>BonoLibro</figcaption>
+            </a>
         </figure>
         <img id="menu-icon" src="images\menu.svg" alt="">
 
         <nav>
             <ul>
-                <li><a href="#">Läser nu</a></li>
+                <li><a href="myBooksV2.php?list=tbr">Vill läsa</a></li>
                 <hr>
-                <li><a href="#">Har läst</a></li>
+                <li><a href="myBooksV2.php?list=hbr">Har läst</a></li>
                 <hr>
-                <li><a href="#">Vill läsa</a></li>
+                <li><a href="myBooksV2.php?list=favourites">Favoriter</a></li>
                 <hr>
+                <?php
+                $numberOfLists = $userinfo['11'];
+                if ($numberOfLists != NULL) {
+                    $lists = getLists($_SESSION['userId']);
+                    if (!contains(';:', $lists['1'])) {
+                        $delfkn = "deleteList('" . $lists['1'] . "');";
+                        echo '<li><a href=myBooksV2.php?list=' . $lists['1'] . '">' . $lists['1'] . '</a></li>';
+                        echo '<hr>';
+                    } else {
+                        $listname = explode(';:', $lists['1']);
+                        foreach($listname as $i) {
+                            $delfkn="deleteList('".$i."');";
+                            echo '<li><a href="myBooks.php?list='.$i.'">'.$i.'</a></li>';
+                            echo '<hr>';
+                        }
+                    }
+                }
+                ?>
                 <li><a href="#">+ Skapa ny lista</a></li>
                 <li>
                     <form action="includes/logout.inc.php" method="post">
@@ -47,6 +71,17 @@ include 'includes/getDb.inc.php';
         <div class="list-side">
             <div class="container-list">
                 <h1>Läser nu</h1>
+
+                <form action="" id="form-perPage">
+                    <label for="perPage">per sida</label>
+                    <select name="perPage" id="perPage" onchange="$('form-perPage').submit()">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                    </select>
+                </form>
+
                 <hr>
                 <div class="list-bookItem">
                     <img src="images/greatgatsby.jpg" class="book-cover" alt="">
@@ -112,7 +147,23 @@ include 'includes/getDb.inc.php';
                 </div>
                 <hr>
 
+                <div class="list-bookItem">
+                    <img src="images/harrypotter.jpg" class="book-cover" alt="">
+
+                    <div class="bookItem-description">
+                        <a href="book.php" class="list-bookTitle">Harry Potter</a>
+                        <p>Written by</p>
+                        <a href="author.php" class="list-bookAuthor">J.K. Rowling</a>
+                    </div>
+
+                    <div class="bookItem-data">
+                        <p>Betyg: 4.1 / 5</p>
+                        <p>Lades till 'datum'</p>
+                    </div>
+                </div>
             </div>
+
+
         </div>
 
         <div class="ads">
